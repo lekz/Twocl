f0bff26db187c48f23837fa5aad01aeb6b475725
Kosmynin Aleksandr <kosmunin@gmail.com>
2011-04-05 19:27:42 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twocl::Application.config.secret_token = 'b1d203ae6540e6f22da22ac7c03718cfa019533a4b8675007d4a26e6249a929809651bbb81e73292249d038c62018178bf99ebfc061c7a9840592ebd3924a45d'
